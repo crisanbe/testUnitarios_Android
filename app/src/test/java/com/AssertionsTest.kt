24dd40a5949060e079aa139e65c4c9a0e6f47431
crisanbe <55001756+crisanbe@users.noreply.github.com>
2022-01-25 16:37:44 -0500

    (SESION 2 Junit fundamentos):Aserciones Booleanas assertFalse y assertTrue.

--- app/src/test/java/com/AssertionsTest.kt
+++ app/src/test/java/com/AssertionsTest.kt
@@ -22,4 +22,12 @@ class AssertionsTest{
         assertEquals(name, assertions.getName())
         assertNotEquals(otroname, assertions.getName())
     }
+    @Test
+    fun checkHumanTest(){
+        val assertions = Assertions()
+        val bot = User("64 bits",1, false )
+        val juan = User("Juan",18, true )
+        assertFalse(assertions.checkHuman(bot))
+        assertTrue(assertions.checkHuman(juan))
+    }
 }
