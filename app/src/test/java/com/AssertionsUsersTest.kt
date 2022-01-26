bd39f50cff9bab0df7629f6d5fc7a49f0524035f
crisanbe <55001756+crisanbe@users.noreply.github.com>
2022-01-26 14:45:48 -0500

    (Sesion 2 Junit):Complemento @Before una forma de declarar de forma global los mismos recursos para test que vayamos a implementar

---
+++ app/src/test/java/com/AssertionsUsersTest.kt
@@ -0,0 +1,43 @@
+package com
+
+import org.junit.Assert.*
+import org.junit.Before
+
+import org.junit.Test
+
+class AssertionsUsersTest {
+
+
+    // De tipo User
+    private lateinit var bot: User
+    private lateinit var juan: User
+
+    //todo @Before complemento para nuestras pruebas
+    //Esta se va ejecutar antes de cada prueba
+    //Una forma de declarar nuestras variable globales en nuestras pruebas uniterias
+    // y de etsa forma hacer mas pequeños nuestros metodos, que compartan siertos elementos.
+    @Before
+    fun setup() {
+        bot = User("64 bits", 1, false)
+        juan = User("Juan", 18, true)
+    }
+
+    @Test//Nos aseguramos que todos los nombramientos terminen en Test
+    fun checkHumanTest() {
+        val assertions = Assertions()
+        assertFalse(assertions.checkHuman(bot))
+        assertTrue(assertions.checkHuman(juan))
+    }
+
+    //funcion validar que no es nulo (assertNotNull)
+    @Test
+    fun checkNotNullUserTest(){
+        assertNotNull(juan)
+        //prueba simple con assertNotNull.
+        //----------------------
+        //prueba instanciando el metodo checkHuman para validar que no es nulo
+        val assertions = Assertions()
+        assertNotNull(assertions.checkHuman(juan))
+    }
+
+}
